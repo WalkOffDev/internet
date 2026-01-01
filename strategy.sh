40d4a72726241d280aaf1e08b032b8f414c2e462
WalkOffDev <38154010+WalkOffDev@users.noreply.github.com>
2026-01-01 23:34:56 +0300

    Update strategy.sh

--- strategy.sh
+++ strategy.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-if [ -f "/opt/etc/init.d/S51nfqws" ]; then
-    ROOT_DIR=/opt
-    /opt/etc/init.d/S51nfqws stop
+export GITHUB_TOKEN="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"
+
+if [ -d /opt ] && [ -w /opt ]; then
+  WORKDIR=/opt/tmp/nfqws-keenetic/strategy/zapret
 else
-    ROOT_DIR=
-    /etc/init.d/nfqws-keenetic stop
+  WORKDIR=/tmp/nfqws-keenetic/strategy/zapret
 fi
 
-mkdir -p "$ROOT_DIR/tmp/nfqws-keenetic/strategy/zapret"
-cd "$ROOT_DIR/tmp/nfqws-keenetic/strategy"
+cd ~
+rm -rf "$WORKDIR"
+mkdir -p "$WORKDIR"
+cd "$WORKDIR" || { echo "cant cd to $WORKDIR"; exit 1; }
 
-RELEASE_URL=`curl -s https://api.github.com/repos/bol-van/zapret/releases/latest | grep browser_download_url | grep 'embedded.tar.gz' | cut -d '"' -f 4`
-curl -SL# $RELEASE_URL -o zapret.tar.gz
-tar -C zapret -xzf zapret.tar.gz
-cd zapret/*/
+RELEASE_URL="$(
+  curl -fsSL \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github+json" \
+    https://api.github.com/repos/bol-van/zapret/releases/latest |
+  grep -oE '"browser_download_url":\s*"[^"]+embedded\.tar\.gz"' |
+  head -n1 | cut -d '"' -f 4
+)"
+
+echo "Downloading to $WORKDIR"
+curl -fSL --retry 3 --retry-delay 2 -o zapret.tar.gz "$RELEASE_URL"
+tar -xzf zapret.tar.gz
+
+cd zapret-*/
 
 ./install_bin.sh
 SECURE_DNS=1 FWTYPE=iptables SKIP_TPWS=1 ./blockcheck.sh
-
-rm -rf "$ROOT_DIR/tmp/nfqws-keenetic/strategy"
-echo -e "* NOTE: nfqws-keenetic is stopped. Start it manually if necessary! \n"
